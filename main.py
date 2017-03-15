6616a5522a1778541d68de6145346cad796cafbe
Davis Crain <dhcrain@gmail.com>
2017-03-15 08:28:49 -0400

    removed boiler plate

--- main.py
+++ main.py
@@ -1,44 +1,4 @@
-'''
-This is a sample Lambda function that sends an SMS on click of a
-button. It needs one permission sns:Publish. The following policy
-allows SNS publish to SMS but not topics or endpoints.
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "sns:Publish"
-            ],
-            "Resource": [
-                "*"
-            ]
-        },
-        {
-            "Effect": "Deny",
-            "Action": [
-                "sns:Publish"
-            ],
-            "Resource": [
-                "arn:aws:sns:*:*:*"
-            ]
-        }
-    ]
-}
 
-The following JSON template shows what is sent as the payload:
-{
-    "serialNumber": "GXXXXXXXXXXXXXXXXX",
-    "batteryVoltage": "xxmV",
-    "clickType": "SINGLE" | "DOUBLE" | "LONG"
-}
-
-A "LONG" clickType is sent if the first press lasts longer than 1.5 seconds.
-"SINGLE" and "DOUBLE" clickType payloads are sent for short clicks.
-
-For more documentation, follow the link below.
-http://docs.aws.amazon.com/iot/latest/developerguide/iot-lambda-rule.html
-'''
 
 from __future__ import print_function
 
